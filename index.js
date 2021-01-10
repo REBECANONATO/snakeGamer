719a29adad29b511a9c966aa3344cc674e7a65c8
rebecanonato <rebecanonato89@gmail.com>
2021-01-10 14:54:35 -0300

    snake gamer 1

---
+++ index.js
@@ -0,0 +1,71 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+
+function createBG() {
+    context.fillStyle = "red";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillReact(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+
+document.addEventListener("kedown", update);
+
+
+function update(event) {
+
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
+
+function startGame() {
+
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].x = 0;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].x = 16 * box;
+
+
+
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snake += box;
+    if (direction == "left") snake -= box;
+    if (direction == "up") snake -= box;
+    if (direction == "down") snake += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+
+let game = setInterval(startGame, 100);
